734cd878b25338ddc6efa9a35a507ac2034ca852
Brain-drain <yuliagreat@mail.ru>
2018-09-09 22:34:07 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println ("\tМеня зовут Юлия Уретя, мне 21");
+        System.out.print(" Я учусь в ПОЛИТЕХе на третьем курсе\n");
+        System.out.println("\bРанее училась в ТГНП ОНАПТ на связиста");
+
+
+    }
+}
